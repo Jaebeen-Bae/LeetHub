3c42c9bc27d7f09a6c5dfcaf923b5b88ae80a21c
Jaebeen <97034016+Jaebeen-Bae@users.noreply.github.com>
2023-01-02 20:47:10 +0900

    Time: 1 ms (39.45%), Space: 40.7 MB (71.10%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            ans.add(root.val);
+
+            if (root.right != null) stack.push(root.right);
+            if (root.left != null) stack.push(root.left);
+        }
+
+        return ans;
+    }
+}
