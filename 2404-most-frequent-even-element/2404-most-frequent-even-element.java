680522becc8a73aa90bb9281d2f9ba8c12d66bcb
Jaebeen <97034016+Jaebeen-Bae@users.noreply.github.com>
2023-01-06 16:41:23 +0900

    Time: 28 ms (74.28%), Space: 58.8 MB (54.10%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -2,19 +2,19 @@ class Solution {
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer,Integer>numberCounts =new HashMap<>();
         int result=-1 ,maxFreq=-1;
-        for(int num:nums)
-            if(num%2==0)                    
-                numberCounts.put(num,numberCounts.getOrDefault(num,0)+1);                                               
-            
-            
-        for(Integer num:numberCounts.keySet()){      
-            if(numberCounts.get(num)>maxFreq){
+        for(int num:nums) {
+            if(num%2==0) {           
+              numberCounts.put(num,numberCounts.getOrDefault(num,0)+1);
+            }
+        }        
+        for(Integer num:numberCounts.keySet()) {      
+            if(numberCounts.get(num)>maxFreq) {
                 maxFreq=numberCounts.get(num);
                 result=num;
             }
-            else if(numberCounts.get(num)==maxFreq && result>num)
+            else if(numberCounts.get(num)==maxFreq && result>num) {
                 result=num;                        
-                                
+            }                    
         }
         return result;                
     }
